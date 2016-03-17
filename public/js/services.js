4a6d1159e5376156302bae6ec8fb46d8441bc473
Apple <tiffine.koch@gmail.com>
2016-03-17 09:15:38 -0700

    hw submission

--- public/js/services.js
+++ public/js/services.js
@@ -1,8 +1,7 @@
 'use strict';
-//
+
 var app = angular.module('routingApp');
-//
-//
+
 app.service('ScriptService', function($http) {
   this.fetch = function() {
     return $http.get('/scripts');
